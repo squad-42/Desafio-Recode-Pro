af501c7e33320d06e6abee133bc95f208bc3bcb8
diegophb <prdiegopi@gmail.com>
2023-08-24 17:01:14 -0300

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -22,7 +22,7 @@ export const menuItens = [
   {
     icon: "question",
     label: "Sobre",
-    link: "sobre"
+    link: "quem-somos"
   },
 
 ]
@@ -199,5 +199,23 @@ export const comments = [
     userPic: 3,
     date: "20-08-2023",
     text: "Excelente didática."
+  },
+]
+
+
+export const sobreCards=[{
+  img:"missão",
+  title:"MISSÃO",
+  text:"Nossa missão é clara e ambiciosa: proporcionar a cada indivíduo a chance de dominar o inglês e expandir suas perspectivas. Acreditamos que o inglês é mais do que apenas um idioma - é uma ferramenta que abreportas para oportunidades educacionais, profissionais e culturais. Movidos por essa crença, estamos empenhados em oferecer uma plataforma de aprendizado que não só ensina, mas também capacita, inspira e conecta.",
+  },
+{
+  img:"visao",
+  title:"VISÃO",
+  text:"Visualizamos uma plataforma inovadora, reconhecida pela sua eficácia e abordagem inclusiva. Queremos ser a primeira escolha para quem busca aprender inglês, oferecendo recursos interativos e personalizados que se adaptam às necessidades individuais dos alunos. Almejamos construir uma comunidade diversificada e unida, unindo culturas e possibilitando uma compreensão mais profunda entre as pessoas por meio da língua inglesa.",
+  },
+{
+  img:"valores",
+  title:"VALORES",
+  text:"<b> Acessibilidade Universal:</b> Acreditamos que a educação de qualidade deve ser acessível a todos, independentemente de sua origem socioeconômica. </p><p><b> Inclusão:</b> Valorizamos a diversidade e a inclusão, criando um ambiente de aprendizado onde todos os alunos se sintam bem-vindos e respeitados, independentemente de sua origem étnica, cultural ou social.</p>",
   },
 ]
